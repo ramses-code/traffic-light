88777a752d853571c59a9c82a7997d0a76890a86
Ramses Cevallos <cevallosramses@gmail.com>
2022-06-24 03:08:55 +0000

    created traffic-light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [color, setColor] = useState(null);
+	const [purple, setPurple] = useState(false);
+	let text = '';
+
+	if (purple == false) text = "Add Light";
+	else text = "Remove Light"
+
+	function handleChange(){
+		const colors = ["red", "yellow", "green"];
+		let colorIndex = 0;
+
+	let interval = setInterval(() => {
+		if (colorIndex <= 2) {
+			setColor(colors[colorIndex]);
+			colorIndex ++;
+		}
+		else {
+			setColor("red");
+			colorIndex = 0;
+		}
+		console.log(colors[colorIndex])
+	},1000);
+
+		
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main-container">
+			<div className="top"></div>
+			<div className="container">
+				<div>
+					<button onClick={() => setColor("red")} type="button" className={color === "red" ? "glow bg-danger" : "btn btn-danger"}></button>
+				</div>
+				<div>
+					<button onClick={() => setColor("yellow")} type="button" className={color === "yellow" ? "glow bg-warning" : "btn btn-warning"}></button>
+				</div>
+				<div>
+					<button onClick={() => setColor("green")} type="button" className={color === "green" ? "glow bg-success" : "btn btn-success"}></button>
+				</div>
+				<div>
+					<button onClick={() => setColor("purple")} id="purple" type="button"  className={purple === true ? "btn btn-info d-block" : "btn btn-info d-none"}></button>
+				</div>
+			</div>
+			<div className="bonus">
+				<button onClick={() => purple === false ? setPurple(true) : setPurple(false)} data-target="#purple" type="button" className="btn btn-dark">{text}</button>
+				<button onClick={() => handleChange()} type="button" className="btn btn-secondary">On/Off</button>
+			</div>
 		</div>
 	);
 };
